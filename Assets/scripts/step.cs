3d56990ff24f6a9c5a2d0f415a6b7afe16894a5e
Diwen <lidiwengz@gmail.com>
2020-07-13 16:48:59 -0400

    step control script

--- Assets/scripts/step.cs
+++ Assets/scripts/step.cs
@@ -8,15 +8,15 @@ public class step : MonoBehaviour
     public Vector3 location;
     
     //float and dive will only got one input
-    public void step(string act){
-        this.action = act;
-    }
+    //public void step(string act){
+    //    action = act;
+    //}
 
     //move and attack will take two input
-    public void step(string act,Vector3 locate){
-        this.action = act;
-        this.location = locate;
-    }
+    //public void step(string act,Vector3 locate){
+    //    action = act;
+    //    location = locate;
+    //}
 
     public string GetStep(){
         return action;
